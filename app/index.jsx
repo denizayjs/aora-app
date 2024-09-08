80d8ebd281bedcd54ab764d10320e7c4b6036ba0
deniz ay <denizayjs@gmail.com>
2024-09-08 18:31:04 +0300

    fix: change font

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import { Link } from 'expo-router';
 export default function App() {
   return (
     <View className='flex-1 items-center justify-center bg-primary'>
-      <Text className='text-3xl text-white font-pblack'>Aora!</Text>
+      <Text className='text-3xl text-white font-pbold'>Aora!</Text>
       <StatusBar style='auto' />
       <Link href='/profile' style={{ color: 'blue' }}>
         Go to Profile
