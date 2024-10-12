d6495acea4b23cf35a85ee898e4a4cef90f217a1
deniz ay <denizayjs@gmail.com>
2024-10-12 12:10:28 +0300

    feat: create VideoCard component

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -14,26 +14,21 @@ import SearchInput from '../../components/SearchInput';
 import EmptyState from '../../components/EmptyState';
 import { getAllPosts } from '../../lib/appwrite';
 import useAppwrite from '../../lib/useAppwrite';
+import VideoCard from '../../components/VideoCard';
 
 const Home = () => {
-  const [refreshing, setRefreshing] = useState(false);
-
-  const { data: posts } = useAppwrite(getAllPosts);
+  const { data: posts, isLoading, refetch } = useAppwrite(getAllPosts);
 
   const onRefresh = async () => {
-    setRefreshing(true);
-    // re call videos
-    setRefreshing(false);
+    await refetch();
   };
 
   return (
     <SafeAreaView className='bg-primary  h-full'>
       <FlatList
-        data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+        data={posts}
         keyExtractor={(item) => item.$id}
-        renderItem={({ item }) => (
-          <Text className='text-3xl text-white'>{item.id}</Text>
-        )}
+        renderItem={({ item }) => <VideoCard video={item} />}
         ListHeaderComponent={() => (
           <View className='my-6 px-4 space-y-6'>
             <View className='justify-between items-start flex-row mb-6'>
@@ -69,7 +64,7 @@ const Home = () => {
           />
         )}
         refreshControl={
-          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          <RefreshControl refreshing={isLoading} onRefresh={onRefresh} />
         }
       />
     </SafeAreaView>
